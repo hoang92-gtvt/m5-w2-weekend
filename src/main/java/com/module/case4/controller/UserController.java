36f19a1048f9374370e82629f1f361fc4014ebc5
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-07-07 14:48:54 +0700

    add method findOne

--- src/main/java/com/module/case4/controller/UserController.java
+++ src/main/java/com/module/case4/controller/UserController.java
@@ -2,16 +2,16 @@ package com.module.case4.controller;
 
 
 import com.module.case4.model.user.User;
+import com.module.case4.model.user.UserForm;
 import com.module.case4.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -32,6 +32,17 @@ public class UserController {
         }
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
-
+    @GetMapping("/admin/findOne/{id}")
+    public ResponseEntity<User> findOneUser(@PathVariable Long id){
+        Optional<User> user = userService.findById(id);
+        if(!user.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user.get(), HttpStatus.OK);
+    }
+//    @PostMapping("/create")
+//    public ResponseEntity<User> createUser(@RequestBody UserForm  userForm){
+//        User user
+//    }
 
 }
--- src/main/java/com/module/case4/service/IUserService.java
+++ src/main/java/com/module/case4/service/IUserService.java
@@ -1,11 +1,12 @@
 package com.module.case4.service;
 
 import com.module.case4.model.user.User;
+import com.module.case4.model.user.UserForm;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface IUserService {
+public interface IUserService  {
     Optional<User> findByUsername(String name); //Tim kiem User co ton tai trong DB khong?
     Boolean existsByUsername(String username); //username da co trong DB chua, khi tao du lieu
     Boolean existsByEmail(String email); //email da co trong DB chua
@@ -14,6 +15,8 @@ public interface IUserService {
     Iterable<User> findAll();
     Optional<User> findById(Long id);
     void delete(Long id);
+
+    User changeUserForm(UserForm userForm);
 
 
 
