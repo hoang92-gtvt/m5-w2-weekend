6313925d1970edc8dbfa0e9849061b3a394fc561
deepvn98 <deepvn988@gmail.com>
2021-07-07 15:49:59 +0700

    done crud đơn giản

--- src/main/java/com/module/case4/model/course/DetailCourse.java
+++ src/main/java/com/module/case4/model/course/DetailCourse.java
@@ -20,7 +20,7 @@ public class DetailCourse {
     private Long id;
 
     @ManyToOne
-//    @JoinColumn(name = "course_id")
+    @JoinColumn(name = "course_id")
     private Course course;
 
     private Date startTime;
@@ -31,9 +31,11 @@ public class DetailCourse {
     private Status status = Status.WAIT;
 
     @ManyToOne
+    @JoinColumn(name = "userTeacher_id")
     private User userTeacher;
 
     @ManyToOne
+    @JoinColumn(name = "userStudent_id")
     private User userStudent;
 
 }
