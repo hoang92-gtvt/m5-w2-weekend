efeb7a4a6aa76c35d5a31eb75f8d18e5d93d8f12
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-07-07 10:52:25 +0700

    start controller user

--- src/main/java/com/module/case4/controller/UserController.java
+++ src/main/java/com/module/case4/controller/UserController.java
@@ -1,10 +1,30 @@
 package com.module.case4.controller;
 
 
+import com.module.case4.model.user.User;
+import com.module.case4.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    public IUserService userService;
+
+//    @ModelAttribute("currentAdmin")
+//    public  User getUser(){
+//        return userService.
+//    }
+
+//    @GetMapping("/findAnd")
+//    public ResponseEntity<User> findAll(){
+//
+//    }
 
 
 }
